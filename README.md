5936625032e2e8031bab7fa53d43eb8372251233
Dave Josephsen <dave@skeptech.org>
2015-07-08 11:55:45 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # skinnywhale
 Skinnywhale helps you make smaller (as in megabytes) Docker containers
+
+It's a shell script that you can use to isolate the runtime environment you
+need to execute something like a python/ruby/java program in a container. The
+images you make with Skinnywhale are normally hundreds of megs smaller than
+their bloated counterparts.  Here's how you use it:
+
+## step 1. Start with a normal fat image
+
+``` docker run -ti ubuntu ```
+
+## step 2. Make a container that has the runtime you need in it
+
+``` apt-get update
+apt-get install python3
+exit
+
+docker ps -a
+<copy the container ID you just created eg.. 8efbc5497abb>
+```
+
+## step 3. isolate the runtime layer you just added
+
+```
+skinnywhale 8efbc5497abb
+```
+
+Skinnywhale requires that you're using an *aufs-backed* docker that is version
+1.6 or later (there are bugs earlier than that which cause "file not found"
+errors on images imported from tarballs)
+
+You can see what kind of back-end you have with:
+
+```
+#  docker info
+Containers: 2
+Images: 7
+Storage Driver: aufs
+ Root Dir: /var/lib/docker/aufs
+ Backing Filesystem: extfs
+ Dirs: 11
+ Dirperm1 Supported: false
+```
+
+If you're on ubuntu and your docker is not using aufs, it's probably because
+you're missing the linux-image-extra package. Try running: 
+
+```
+apt-get -y install linux-image-extra-$(uname -r)
+service docker restart
+docker info
+```
+
+## How does this work?
+Skinnywhale is pretty simple, it finds the aufs path for the container ID you
+give it, and walks through it looking for binary files that are dynamically
+linked. Skinnywhale resolves each of these dependencies by copying the linked
+library files in from the parent image's aufs directory. It then tars up the
+resulting filesystem and *docker import*s it. 
+
+There are two caveats to this process. First, some runtime environments (like
+the oracle JDK), contain files that link to libs that may not be installed on
+the client (if, for example you're using a base-image that doesn't have Xorg
+installed, the JDK will have binary files that are linked to X11 libs that
+don't exist on the base image). This is not usually a problem; as long as your
+app actually works with the base image you're trying to use, it should work
+post-skinnywhale. 
+
+Second, Skinnywhale can't detect if your runtime uses dlOpen(). You're on
+you're on there I'm afraid. I've been using skinnywhale for python and java
+stuff at work, and in practice I've found this not to be an issue.  If it
+becomes an issue I might think about adding options to just wholesale copy
+across the parent /lib /usr/lib et all..
---
+++ skinnywhale.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+# Isolate the given top-level docker container
+
+AUFS='/var/lib/docker/aufs/diff'
+
+function usage {
+#print usage and exit
+        echo "usage: whalestarve <container ID>"
+	exit 42
+}
+
+function error {
+#print an error and exit
+        echo "ERROR: ${@}" 1>&2
+	exit 42
+}
+
+function debug {
+	[ "${DEBUG}" ] && echo "DEBUG:: ${@}" 1>&2
+}
+
+function getFullHash {
+#return the full hash for a given container ID
+	basename $(find ${AUFS} -maxdepth 1 -name ${1}* | grep -v 'init')
+}
+
+function findDep {
+#walk the images starting at $1 looking for $2
+	if [ -f "${AUFS}/${1}/${2}" ]
+	then
+		echo "${AUFS}/${1}/${2}"
+	else
+		fdPARENT=$(docker inspect ${1} | egrep 'Parent":' | sort | uniq | cut -d':' -f2 | tr -d '", ')
+		for P in $(docker history ${fdPARENT} | sed -ne '2,$p' | sed -e 's/ .*//')
+		do
+			IMAGE=$(getFullHash ${P})
+			debug "checking for ${AUFS}/${IMAGE}/${2}"
+			if [ -f "${AUFS}/${IMAGE}/${2}" ]
+			then
+				debug "Found $2 at ${AUFS}/${IMAGE}/${2}"
+				echo "${AUFS}/${IMAGE}/${2}"
+				break
+			fi	
+		done
+	fi
+}
+
+function panicAndFindDep {
+#be stupid and just find the dang thing
+	DEP=$(basename ${1})
+	find ${AUFS} -name ${DEP} | head -n1
+}
+
+#lets kick this pig
+debug "sanity checking"
+[ "${1}" ] || usage 
+
+SD=$(docker info | grep Storage)
+if ! echo ${SD} | grep -q aufs
+then
+	echo "Sorry, docker needs to be running with aufs support"
+	exit 42
+fi
+
+LAYER=$(getFullHash ${1})
+debug "layer is ${LAYER}"
+LPATH="${AUFS}/${LAYER}"
+debug "LPATH is ${LPATH}"
+PARENT_NAME=$(docker ps -a | grep ${1}| sed -e 's/ \+/ /g' | cut -d\  -f2)
+PARENT=$(docker images | grep "^${PARENT_NAME}" | sed -e 's/ \+/ /g' | cut -d\  -f3)
+debug "PARENT is ${PARENT}"
+DEPS=''
+
+#copy over the linker
+LINKER=$(findDep $(getFullHash ${PARENT}) /usr/bin/ldd)
+debug "cp -nv ${LINKER} ${LPATH}/usr/bin"
+cp -nv ${LINKER} ${LPATH}/usr/bin
+
+#recursively grab the library dependencies for every binary
+for FILE in `find ${LPATH} -type f`
+do
+	if file ${FILE} | grep -q 'dynamically linked'
+	then
+		debug "processing deps for ${FILE}"
+		D=$(ldd ${FILE} | sed -e 's/\(^[^/]\+\)\([^ ]\+\).*/\2/' | grep '/')
+		DEPS=$(printf "%s\n%s" "${DEPS}" "${D}" | sort | uniq)
+	fi
+done
+
+#copy in the library dependencies
+debug "DEPENDENCIES is ${DEPS}"
+for DEP in ${DEPS}
+do
+	mkdir -p ${LPATH}/$(dirname ${DEP})
+	SOURCE=$(findDep ${PARENT} ${DEP})
+	if [ -z "${SOURCE}" ]
+	then
+		SOURCE=$(panicAndFindDep ${DEP})
+	fi
+	if [ -z "${SOURCE}" ]
+	then
+		echo "ERROR!!!, can't find ${DEP} in the parent images"
+		#exit 42
+	else
+		cp -nv ${SOURCE} ${LPATH}/${DEP}
+	fi
+done
+
+#tar it all up and re-dockerize it
+cd ${LPATH}
+tar -c . --exclude=/var/cache | docker import - starved${1}
+debug "tar -cv . | docker import - starved${1}"
+echo "--- Skinny whale is positively starving ---"
+echo '
+                    ##        .            
+              ## ## ##       ==            
+           ## ## ## ##      ===            
+       /""""""""""""""""\___/ ===        
+      /	rX
+  ~~~{ /\ ~ ~~~ ~~~~ ~~ ~ /  ===- ~~~   
+       \______////////////__/            
+'
